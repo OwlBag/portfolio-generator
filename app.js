98fc94b20f66e24821059fdd427255f4e7802a53
OwlBag <Owlbag0410@gmail.com>
2022-05-08 23:08:27 -0400

    cleaned up code with custom Promises

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ promptUser()
         return generatePage(portfolioData);
     })
     .then(pageHTML => {
-        return fs.writeFile(pageHTML);
+        return writeFile(pageHTML);
     })
     .then(writeFileResponse => {
         console.log(writeFileResponse);
